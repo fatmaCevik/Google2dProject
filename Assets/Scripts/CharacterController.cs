9b3f671aacf70722aca512fbcba29e3455f92d07
fatmaCevik <cevikfatma93@gmail.com>
2024-02-06 04:35:58 +0300

    Introduction to Character Animation and Control 1

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 0.0f;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+
+    private void Start()
     {
-        
+        // rb = GetComponent<Rigidbody2D>(); Burada Caching yapýyoruz. Rigidbody2D'yi Cach ediyoruz. Karaktere ait componenti tanýmlamak.
+        // animator = GetComponent<Animator>(); Caching. Animator Cach edilir. Karaktere ait Animator Componenti tanýmlanýr.
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space))
+        {
+            speed = 1.0f;
+            Debug.Log("Hýz 1.0f");
+        }
+        else
+        {
+            speed = 0.0f;
+            Debug.Log("Hýz 0.0f"); 
+        }
+
+        animator.SetFloat("Speed", speed);
+        rb.velocity = Vector2.right * speed; //rb.velocity = new Vector2(x:speed, y:0f);
     }
 }
